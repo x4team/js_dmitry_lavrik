c81ebcffee78d85cf76020bcd7fcb84c1ccf9e95
Dmitry Rakov <x4team@gmail.com>
2017-11-26 23:24:44 +0300

    Add js 3

--- js/script.js
+++ js/script.js
@@ -27,24 +27,24 @@ var i = 0;
 
 
 btn_prev.onclick = function() {
-	images[i].style.display = 'none';
+	images[i].className = '';
 	i--;
 
 	if (i<0) {
 		i = images.length - 1;
 	}
 
-	images[i].style.display = 'block';
+	images[i].className = 'showed';
 
 }
 
 btn_next.onclick = function() {
-	images[i].style.display = 'none';
+	images[i].className = '';
 	i++;
 
 	if (i>=images.length) {
 		i = 0;
 	}
 
-	images[i].style.display = 'block';
+	images[i].className = 'showed';
 }
