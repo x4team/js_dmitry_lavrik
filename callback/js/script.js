fb568ec8cbd5ef8efbcbc4ce46a457836840f5b1
Dmitry Rakov <x4team@gmail.com>
2017-11-27 19:33:46 +0300

    Add last

--- callback/js/script.js
+++ callback/js/script.js
@@ -1,50 +1,12 @@
-/*
-	свойства
-	события
-	методы
 
 
 
+var item = document.querySelector('.items .item');
 
-var text = document.querySelector('#text');
-
-text.onclick = function(){
-	
-	//text.innerHTML = 'ура'; //заменить текст по клику 
-	text.innerHTML = 'ура'; //добавить текст по клику 
-	text.className = 'test';
-	text.style.color = 'red'; //изменить цвет текста по клику
-	text.style.fontSize = '30px';
+item.onclick = activeItem; //ссылка на функцию
+	activeItem(); //скобки в конце означают выполнение прямо сейчас
 }
 
-*/
-
-var btn_prev = document.querySelector('#gallery .buttons .prev');
-var btn_next = document.querySelector('#gallery .buttons .next');
-
-var images = document.querySelectorAll('#gallery .photos img');
-var i = 0;
-
-
-btn_prev.onclick = function() {
-	images[i].className = '';
-	i--;
-
-	if (i<0) {
-		i = images.length - 1;
-	}
-
-	images[i].className = 'showed';
-
-}
-
-btn_next.onclick = function() {
-	images[i].className = '';
-	i++;
-
-	if (i>=images.length) {
-		i = 0;
-	}
-
-	images[i].className = 'showed';
+function activeItem(){
+	this.classList.toggle('item-active');  //переключает классы по клику, если есть класс, то удаляет, если нет, то добавляет
 }
